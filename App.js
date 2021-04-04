95a0b55190c133ab9d335cf623e0cd53a2097f60
Chris Paterson <chrispaterson22@gmail.com>
2021-04-04 13:44:48 +0100

    android local notification

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import * as Notifications from 'expo-notifications'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const triggerNotificationHandler = () => {
+		Notifications.scheduleNotificationAsync({
+			content: {
+				title: 'My first local notification',
+				body: 'This is the first local notification we are sending!',
+			},
+			trigger: {
+				seconds: 10,
+			},
+		})
+	}
+
+	return (
+		<View style={styles.container}>
+			<Button
+				title='Trigger Notification'
+				onPress={triggerNotificationHandler}
+			/>
+			<StatusBar style='auto' />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+})
