5ccccfd1bb5d79b746d90caabce828097dc8f9cb
Chris Paterson <chrispaterson22@gmail.com>
2021-04-04 14:19:45 +0100

    reacting to notifications

--- App.js
+++ App.js
@@ -28,6 +28,25 @@ export default function App() {
 			})
 	}, [])
 
+	useEffect(() => {
+		const backgroundSubscription = Notifications.addNotificationResponseReceivedListener(
+			response => {
+				console.log(response)
+			}
+		)
+
+		const foregroundSubscription = Notifications.addNotificationReceivedListener(
+			notification => {
+				console.log(notification)
+			}
+		)
+
+		return () => {
+			backgroundSubscription.remove()
+			foregroundSubscription.remove()
+		}
+	}, [])
+
 	const triggerNotificationHandler = () => {
 		Notifications.scheduleNotificationAsync({
 			content: {
